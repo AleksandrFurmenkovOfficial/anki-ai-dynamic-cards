181b005a1058889efdff1c66e7198a3b470eebd1
Aleksandr Furmenkov <aleksandr.furmenkov.official@gmail.com>
2023-03-23 12:49:11 +0300

    add implementation

---
+++ README.md
@@ -0,0 +1,18 @@
+# Anki AI Dynamic Cards Add-on
+Anki AI Dynamic Cards is an add-on for Anki that dynamically generates example sentences related to the words you are learning, using the artificial intelligence tool ChatGPT. This add-on provides a unique way to improve the effectiveness of memorizing new words and their usage in context, giving users the ability to practice real-world scenarios.
+
+## Installation
+To install Anki AI Dynamic Cards, you need to download the add-on package `anki-ai-dynamic-cards.zip` and install it manually. You can do this by following these steps:
+- Download the add-on package from the [release page](https://github.com/AleksandrFurmenkovOfficial/anki-ai-dynamic-cards/releases/tag/v1)
+- Find out an Anki add-ons directory. In Anki, select Tools > Add-ons > View Files. This will open the Anki add-ons directory.
+- Extract the downloaded package `anki-ai-dynamic-cards.zip` into the add-ons directory.
+- Restart Anki.
+
+To install the addon directly from the sources you need:
+- Clone the repo
+- Find out an Anki add-ons directory. In Anki, select Tools > Add-ons > View Files. This will open the Anki add-ons directory.
+- `pip install -r openai -t ANKI_ADDONS_DIRECTORY`
+- Restart Anki.
+
+## Usage
+To use Anki AI Dynamic Cards, you need to create a deck with cards that contains just a one word you want to memorize. Anki AI Dynamic Cards will automatically generate an example sentence for the word you are learning, using ChatGPT. You can then review the card as usual.
---
+++ __init__.py
@@ -0,0 +1,51 @@
+import os
+import sys
+from typing import Tuple
+from anki import hooks
+from anki.template import TemplateRenderContext, TemplateRenderOutput
+from aqt import mw
+from PyQt5.QtWidgets import QInputDialog
+
+addon_name = "dynamic_gpt_card_examples"
+addon_path = os.path.join(mw.pm.addonFolder(), addon_name)
+sys.path.append(addon_path)
+import openai
+
+is_api_key_valid = False
+
+def on_card_did_render(output: TemplateRenderOutput, context: TemplateRenderContext):
+    if not is_api_key_valid:
+        return
+
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
+        {"role": "system", "content": "You are an english teacher. You should give 3 examples, each on a new line via <br> tag, for the word which user will provide. The source word should be bold via <b> tags."}, 
+        {"role": "user", "content": output.question_text}])
+    output.question_text = completion.choices[0].message.content
+
+api_key_file = os.path.join(addon_path, "api_key.txt")
+api_key = ""
+try:
+    with open(api_key_file, "r") as file:
+        api_key = file.read()
+except:
+    pass
+
+if api_key == "":
+    try:
+        api_key, ok = QInputDialog.getText(None, "Provide OpenAI api key", "OpenAI key:", text="sk-XXXXXXXXXXX")
+        if ok:
+            with open(api_key_file, "w") as file:
+                file.write(api_key)
+    except:
+        pass
+
+try:
+    if api_key != "":
+        openai.api_key = api_key
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "system", "content": "Hi!"}])
+        is_api_key_valid = True
+except:
+    if (os.path.isfile(api_key_file)):
+        os.remove(api_key_file)
+
+hooks.card_did_render.append(on_card_did_render)
